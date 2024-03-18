7f7cce71de8478b40377c6ecb8b9d165f4081af9
DaniilRaptanov <daniilraptanov@gmail.com>
2024-03-18 12:56:56 +0200

    feat: addition of /about page to lab-8

--- lab-8/index.js
+++ lab-8/index.js
@@ -9,19 +9,24 @@ const httpServer = http.Server(app);
 const ioServer = io(httpServer);
 
 const INDEX_NAME = './index.html';
+const ABOUT_NAME = './some.txt';
 
 app.use(express.static('public'));
 
-app.get('/', function(_req, res) {
+app.get('/', (_req, res) => {
   res.sendFile(path.resolve(INDEX_NAME));
 });
 
-ioServer.on('connection', function(socket){
+app.get('/about', (_req, res) => {
+  res.sendFile(path.resolve(ABOUT_NAME));
+});
+
+ioServer.on('connection', (socket) => {
   socket.on('send message', function(msg) {
 	ioServer.emit('receive message', msg);
   });
 });
 
-httpServer.listen(3000, function() {
+httpServer.listen(3000, () => {
   console.log('listening on 3000');
 });
